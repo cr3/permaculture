5db55b8f57a834fec438c3742dfbd4586c7648bb
Marc Tardif <marc@interunion.ca>
2023-08-30 21:44:42 -0400

    Add Google spreadsheets web interface

---
+++ permaculture/google/spreadsheets.py
@@ -0,0 +1,29 @@
+"""Google spreadsheets web interface."""
+
+from pathlib import Path
+
+from attrs import define
+from yarl import URL
+
+from permaculture.http import HTTPClient
+
+
+@define(frozen=True)
+class GoogleSpreadsheet:
+    """Google spreadsheet."""
+
+    client: HTTPClient
+    doc_id: str
+
+    @classmethod
+    def from_url(cls, url: URL, cache_dir=None):
+        client = HTTPClient.with_cache_all(url.origin(), cache_dir)
+        doc_id = Path(url.path).parent.name
+        return cls(client, doc_id)
+
+    def export(self, gid=1, fmt="csv"):
+        response = self.client.get(
+            f"spreadsheets/d/{self.doc_id}/export",
+            params={"gid": gid, "format": fmt},
+        )
+        return response.text
---
+++ tests/unit/google/test_spreadsheets.py
@@ -0,0 +1,26 @@
+"""Unit tests for the Google spreadsheets module."""
+
+from unittest.mock import Mock
+
+from yarl import URL
+
+from permaculture.google.spreadsheets import GoogleSpreadsheet
+
+from ..stubs import StubRequestsResponse
+
+
+def test_google_spreadsheet_from_url():
+    """Instantiating from URL should parse the doc ID."""
+    url = URL("https://docs.google.com/spreadsheets/d/test_id/edit")
+    gs = GoogleSpreadsheet.from_url(url)
+    assert gs.doc_id == "test_id"
+
+
+def test_google_spreadsheet_export():
+    """Exporting should GET with gid and format params."""
+    client = Mock(post=Mock(return_value=StubRequestsResponse()))
+    GoogleSpreadsheet(client, "test_id").export("test_gid", "test_format")
+    client.get.assert_called_once_with(
+        "spreadsheets/d/test_id/export",
+        params={"gid": "test_gid", "format": "test_format"},
+    )
