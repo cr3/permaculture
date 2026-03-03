f46461dfb2f8dd73246a77790d529d2675ec68e5
cr3 <cr3@users.noreply.github.com>
2026-03-03 19:59:24 +0000

    Deploying to gh-pages from @ cr3/permaculture@6a034a362a64d213816c8795da77c5743de59352 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.7.dev0+g0864087cc.d20260303',
+    VERSION: '0.0.8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
