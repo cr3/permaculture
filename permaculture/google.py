e0dcedc7b41a3add6143384219ab3faa26f1ee97
Marc Tardif <marc@interunion.ca>
2026-03-08 21:46:06 -0400

    Add sheets method to GoogleSpreadsheet

--- permaculture/google.py
+++ permaculture/google.py
@@ -1,5 +1,6 @@
 """Google spreadsheets web interface."""
 
+import re
 from pathlib import Path
 
 from attrs import define
@@ -21,6 +22,20 @@ class GoogleSpreadsheet:
         session = HTTPSession(url.origin()).with_cache(storage)
         doc_id = Path(url.path).parent.name
         return cls(session, doc_id)
+
+    def sheets(self):
+        """List sheets in the spreadsheet."""
+        response = self.session.get(
+            f"/spreadsheets/d/{self.doc_id}/htmlview",
+        )
+        return {
+            m.group("name"): int(m.group("gid"))
+            for m in re.finditer(
+                r'items\.push\(\{name:\s*"(?P<name>[^"]*)".*?'
+                r'gid:\s*"(?P<gid>\d+)"',
+                response.text,
+            )
+        }
 
     def export(self, gid=1, fmt="csv"):
         response = self.session.get(
